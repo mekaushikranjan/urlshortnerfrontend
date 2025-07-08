25e87dbe931b2cdd86cf03b17d9310a90738f500
Kaushik Ranjan <mritunjaykaushik1803@gmail.com>
2025-07-09 01:02:55 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,6 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = (import.meta.env.VITE_API_BASE_URL?.endsWith('/api')
+  ? import.meta.env.VITE_API_BASE_URL
+  : import.meta.env.VITE_API_BASE_URL + '/api');
 
 export interface URLData {
   id: string;
